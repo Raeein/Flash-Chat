776921b38be40616f7fd12204124b1c5d62deb3e
Raeein Bagheri <raeeinbagheri80@gmail.com>
2021-12-03 10:17:53 -0500

    Add sorting data by date

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -29,8 +29,12 @@ class ChatViewController: UIViewController {
     }
     func loadMessages() -> Void {
 
-        db.collection(K.FStore.collectionName).addSnapshotListener { querySnapshot, error in
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener { querySnapshot, error in
+                
             self.messages = []
+                
             if let e = error {
                 print("There was an issue retrieving data, \(e)")
             } else {
@@ -54,7 +58,8 @@ class ChatViewController: UIViewController {
         if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
             db.collection(K.FStore.collectionName).addDocument(data: [
                 K.FStore.senderField: messageSender,
-                K.FStore.bodyField: messageBody
+                K.FStore.bodyField: messageBody,
+                K.FStore.dateField: Date().timeIntervalSince1970
             ]) { error in
                 if let e = error {
                     print("There was an issue, \(e)")
